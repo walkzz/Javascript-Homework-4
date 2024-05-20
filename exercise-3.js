9db081bd29c99a5691884dc5fce017ee5dcb87da
Pavel Toshinski <pavelce2000@hotmail.com>
2024-05-20 17:09:49 +0200

    Added 2 for loops to iterate over the countries array

--- exercise-3.js
+++ exercise-3.js
@@ -18,3 +18,26 @@
 
 // б. Испечатете ги сите земји што завршуваат со 'ia' и нивното име е поголемо од 7 букви.
 
+const countries = [
+    "Albania",
+    "Bolivia",
+    "Canada",
+    "Denmark",
+    "Ethiopia",
+    "Finland",
+    "Germany",
+    "Hungary",
+    "Ireland",
+    "Japan",
+    "Kenya",
+];
+for (let i = 0; i < countries.length; i++) {
+    if (countries[i].includes("land")) {
+        console.log(countries[i].toUpperCase());
+    }
+}
+for (let i = 0; i < countries.length; i++) {
+    if (countries[i].includes("ia") && countries[i].length > 7) {
+        console.log(countries[i]);
+    }
+}
