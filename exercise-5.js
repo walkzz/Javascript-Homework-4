dab7ae99fb9d3a39a6e9dbb000ee3c3dcfa7aafb
Pavel Toshinski <pavelce2000@hotmail.com>
2024-05-20 18:30:17 +0200

    Added checkGrade function

--- exercise-5.js
+++ exercise-5.js
@@ -9,3 +9,25 @@
 // F: 0-59
 // Да се искористи тернарниот оператор(ternary operator) во функцијата.
 
+function checkGrade(num){
+    if(num > 100 || num < 0){
+        console.log("Invalid number entered");
+    }
+    else if(num <= 100 && num >= 90){
+        console.log(`Grade: A, points: ${num}`);
+    }
+    else if(num <= 89 && num >= 80){
+        console.log(`Grade: B, points: ${num}`);
+    }
+    else if(num <= 79 && num >= 70){
+        console.log(`Grade: C, points: ${num}`);
+    }
+    else if(num <= 69 && num >= 60){
+        console.log(`Grade: D, points: ${num}`);
+    }
+    else if(num <= 59 && num >= 0){
+        console.log(`Grade: F, points: ${num}`);
+    }
+}
+let generateNumber = Math.floor(Math.random() * 100 + 1);
+checkGrade(generateNumber);
