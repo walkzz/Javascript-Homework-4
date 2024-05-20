de8529acc3981ddd4f10dc84b157c0e551651622
Pavel Toshinski <pavelce2000@hotmail.com>
2024-05-20 16:34:39 +0200

    Added forEach() method

--- exercise-1.js
+++ exercise-1.js
@@ -10,3 +10,11 @@
 // го насловот и авторот на секоја книга во конзолата.
 // - Да се искористи методот find() за да се најде првата книга издадена после 2000та година.
 
+const books = [
+    { title: "Book 1", author: "Author A", year: 2000 },
+    { title: "Book 2", author: "Author B", year: 2010 },
+    { title: "Book 3", author: "Author C", year: 2020 },
+]
+books.forEach((book) => {
+    console.log(`Title: ${book.title}, Author: ${book.author}`);
+});
